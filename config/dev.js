fcbcaacd931a4fd76b46550888ab8a3b87e6a4ec
florian lahitte <lahitte.florian@gmail.com>
2019-09-25 09:22:00 -0400

    first commit with mongo google Auth

---
+++ config/dev.js
@@ -0,0 +1,15 @@
+// dev.js - don't commit this !!!
+module.exports = {
+    googleClientID: '452358694598-qm4is5549t8qf2fi8vaj94m9hmh3l2f4.apps.googleusercontent.com',
+    googleClientSecret: 'OPIOYoiuc_Whq5v1qsABDA_f',
+    mongoURI: 'mongodb+srv://flo33:LYOXba9fg1SbgAf0@movie-dev-n0fju.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'rjfhrjvhjrhfjhvlkbjsrklvjbkmlej123fefdgrdfg',
+    secret: 'lfghjhfvljkfbhzrkhjv45454346dfefjazghc',
+    siteUrl: 'http://localhost:3001',
+    instagramClientID: '7c5cde38fe234574be0d33ae2b10a1e5',
+    instagramClientSecret: '9e14dcecaab54abf9e1d5a5b7a8da1a7',
+    facebookClientID:'463575334496256',
+    facebookClientSecret: '3e125e69df570b71c1a6d880384e529f',
+    linkedinClientID: '86sy0pbld2dtyv',
+    linkedinClientSecret: 'UMhL1MEFECFWObdT'
+};
